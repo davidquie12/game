075b7a12de9c7ffca4c770cb9d4b50819df60154
davidquie12 <56687064+davidquie12@users.noreply.github.com>
2023-11-12 00:50:42 +0100

    square

--- venv/src/square.py
+++ venv/src/square.py
@@ -10,11 +10,14 @@ done = False
 red = (255, 0, 0)
 blue = (0, 0, 255)
 
+img = "venv\src\square.png"
+image = pygame.image.load(img).
+
 bg = (127, 127, 127)
 
 x = 0
 y = screen.get_height() // 2
-speed = 1
+speed = 0.5
 
 while not done:
     for event in pygame.event.get():
@@ -23,13 +26,11 @@ while not done:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
             done = True
 
-    screen.fill(bg)
-    font = pygame.font.SysFont("arial", 55)
-    words = font.render("Hello World", True, red)
-    screen.blit(words, (x, y))
+    screen.fill(bg)    
+    screen.blit(image, (x, y))
     x += speed
 
-    if x >= width - words.get_width() or x <= 0:
+    if x >= width - image.get_width() or x <= 0:
         speed = -speed
 
     pygame.display.update()
