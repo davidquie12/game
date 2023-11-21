85542f0c1698d21453c01e425412a992b5aeb097
davidquie12 <56687064+davidquie12@users.noreply.github.com>
2023-11-21 05:18:36 +0100

    core agme works

--- venv/src/pong.py
+++ venv/src/pong.py
@@ -1,139 +1,164 @@
 import pygame
 from collections import namedtuple
-from random import randint
+from random import randint, uniform
+import math
 
 class Unit:
+    def __init__(self,pos : pygame.Vector2,dir_vec : pygame.Vector2,color,speed,
+                 width = None ,height = None,radius = None):
+        self.pos = pos
+        self.width = width
+        self.height = height
+        self.radius = radius
+        self.color = color
+        self.speed = speed
+        self.dir_vec = dir_vec
+        
+    def draw(self,screen : pygame.surface):
+        pass
     
-    def __init__(self, position, dir_vect):
-        self.position = {"x": position[0], "y": position[1]}
-        self.dir_vect = {"x": dir_vect[0], "y": dir_vect[1]}
+    def update(self):
+        pass
+    
         
 
-    def load(self, image, surface):
-        surface.blit(image, (self.position["x"], self.position["y"]))
-        
-    def scale_image(self, image, scale_factor):
-        new_width = int(image.get_width() * scale_factor)
-        new_height = int(image.get_height() * scale_factor)
-        return pygame.transform.scale(image, (new_width, new_height))
     
 class Paddle(Unit):
     
-    max_speed = 1
-    img = "venv/src/paddle.png"
-    image = pygame.image.load(img)
-    height = image.get_height()
-    width = image.get_height()
-    def __init__(self, position, dir_vect):
-        super().__init__(position, dir_vect)
+    def __init__(self,pos : pygame.Vector2,dir_vec : pygame.Vector2,color,speed):
+        super().__init__(pos,dir_vec,color,speed)
+        self.game = game 
+        self.pos = pos 
+        self.dir_vec = dir_vec
+        self.color = color
+        self.speed = speed
+        self.height = 80
+        self.width = 30
+        self.rect = pygame.Rect(self.pos.x, self.pos.y, self.width, self.height)
+        self.is_moving = False
 
-    def scale_image(self, scale_factor):
-        return super().scale_image(self.image, scale_factor)
-
-
+    def draw(self, screen : pygame.surface):
+        pygame.draw.rect(screen,self.color,self.rect)
+        
+    def update(self):
+        self.rect.y += self.dir_vec.y
+        
+        # Check and adjust if the paddle hits the screen boundaries
+        if self.rect.top <= 0:
+            self.rect.y = 0
+        elif self.rect.bottom >= screen.get_height():
+            self.rect.y = screen.get_height() - self.height
+        
 class Ball(Unit):
-    radius = 5.0
-    start_pos = [0, 0]
-    img = "venv/src/ball.png"
-    image = pygame.image.load(img)
-    width = image.get_width()
-    height = image.get_height()
-    max_speed = 3
+    
 
-    def __init__(self, position, dir_vect):
-        super().__init__(position, dir_vect)
-        
-    def scale_image(self, scale_factor):
-        return super().scale_image(self.image, scale_factor)
-    
-class Boundary:
-    
-    def __init__(self, width=640, height=640):
-        self.width = width
-        self.height = height
-        self.top = 0
-        self.bottom = height -1
-        self.left = 0
-        self.right = width - 1
+    def __init__(self,center : pygame.Vector2,radius : int ,dir_vec : pygame.Vector2,color,speed = 4):
+        super().__init__(center,dir_vec,color,speed) 
+        self.dir_vec = dir_vec
+        self.color = color
+        self.speed = speed
+        self.center = center
+        self.radius = radius
 
-    
-class Collision:
-    @staticmethod
-    def collide_with(L_pad: Paddle, R_pad: Paddle, ball: Ball):
-        field = Boundary()
-        
-        # Ball and Paddle Collision with left paddle
+    def check_collision_with_left_paddle(self, L_pad: Paddle):
         if (
-            ball.position["x"] <= L_pad.position["x"] + L_pad.image.get_width()
-            and ball.position["y"] >= L_pad.position["y"]
-            and ball.position["y"] + ball.image.get_height() <= L_pad.position["y"] + L_pad.image.get_height()
+            self.dir_vec.x < 0  # Check if the ball is moving left
+            and L_pad.rect.colliderect(pygame.Rect(self.center.x - self.radius, self.center.y - self.radius, self.radius * 2, self.radius * 2))  # Check for collision
         ):
-            ball.dir_vect["x"] *= -1
-            hit_pos = (ball.position["y"] + ball.image.get_height() / 2) - L_pad.position["y"]
-            paddle_center = L_pad.image.get_height() / 2
-            normalized_hit_pos = (hit_pos - paddle_center) / paddle_center
-            ball.dir_vect["y"] = normalized_hit_pos
+            self.dir_vec.x *= -1  
 
-        # Ball and Paddle Collision with right paddle
-        if (
-            ball.position["x"] + ball.image.get_width() >= R_pad.position["x"]
-            and ball.position["y"] >= R_pad.position["y"]
-            and ball.position["y"] + ball.image.get_height() <= R_pad.position["y"] + R_pad.image.get_height()
-            ):
-            ball.dir_vect["x"] *= -1
-            hit_pos = (ball.position["y"] + ball.height / 2) - R_pad.position["y"]
-            paddle_center = R_pad.height / 2
-            normalized_hit_pos = (hit_pos - paddle_center) / paddle_center
-            ball.dir_vect["y"] = normalized_hit_pos
+            # Check for top/bottom collision
+            if self.center.y - self.radius <= L_pad.rect.top or self.center.y + self.radius >= L_pad.rect.bottom:
+                self.dir_vec.y *= -1  
+                if L_pad.is_moving:
+                    self.speed = 10
+                else:
+                    self.speed = 4
 
-        # Ball and Boundary Collision works
-        if ball.position["y"] <= field.top or ball.position["y"] + ball.image.get_height() >= field.bottom:
-            ball.dir_vect["y"] *= -1
-
-        if ball.position["x"] <= field.left or ball.position["x"] + ball.image.get_width() >= field.right:
-            pygame.quit()
-        #score
-
-        # paddles works
-        if L_pad.position["y"] <= field.top:
-            L_pad.position["y"] = field.top
-          
-        if L_pad.position["y"] + L_pad.image.get_height() >= field.bottom:
-            L_pad.position["y"] = field.bottom - L_pad.image.get_height()
             
-        if R_pad.position["y"] <= field.top:
-            R_pad.position["y"] = field.top
-          
-        if R_pad.position["y"] + R_pad.image.get_height() >= field.bottom:
-            R_pad.position["y"] = field.bottom - R_pad.image.get_height()
+
+    def check_collision_with_right_paddle(self, R_pad: Paddle):
+        if (
+            self.dir_vec.x > 0  # Check if the ball is moving right
+            and R_pad.rect.colliderect(pygame.Rect(self.center.x - self.radius, self.center.y - self.radius, self.radius * 2, self.radius * 2))  # Check for collision
+        ):
+            self.dir_vec.x *= -1  
+
+            # Check for top/bottom collision
+            if self.center.y - self.radius <= R_pad.rect.top or self.center.y + self.radius >= R_pad.rect.bottom:
+                self.dir_vec.y *= -1  
+                if L_pad.is_moving:
+                    self.speed = 10
+                else:
+                    self.speed = 4
+                
+
+
         
+    def draw(self,screen : pygame.surface):
+        pygame.draw.circle(screen,self.color,self.center,self.radius)
+        
+    def update(self):
+        self.pos.x += (self.dir_vec.x * self.speed)
+        self.pos.y += (self.dir_vec.y * self.speed)
+        
+        
+class Collision:      
+    @staticmethod
+    def collide_with_boundary(ball: Ball):
+        
+        if ball.center.x - ball.radius <= 0 or \
+            ball.center.x + ball.radius >= screen.get_width():
+                ball.dir_vec.x *= -1
+                
+        if ball.center.y - ball.radius <= 0 or \
+            ball.center.y + ball.radius >= screen.get_height():
+                ball.dir_vec.y *= -1
+                
+        
+
 
 
 # Initialize Pygame
-pygame.init()
+game = pygame.init()
 
 # Set up the screen
-bounds = Boundary()
-screen = pygame.display.set_mode((bounds.width, bounds.height))
+
+screen = pygame.display.set_mode((640, 640))
 
 # Set up colors
 Color = namedtuple("Color", ("x", "y", "z"))
 dark_blue = (0, 0, 145)
+white = (0,0,0)
 
 # create random generator
-dir_vec_x = randint(-1,1)* Ball.max_speed
-dir_vec_y = randint(-1,1)* Ball.max_speed
-print(f"x: {dir_vec_x} y : {dir_vec_y}")
+# Generate random angles between 0 and 2 * pi
+random_angle = uniform(0, 2 * math.pi)
+
+# Calculate the x and y components of the unit vector using trigonometry
+dir_x = math.cos(random_angle) 
+dir_y = math.sin(random_angle) 
+
+# random start pos
+c_x = randint(30,600) 
+c_y = randint(30,600) 
+
+center = pygame.Vector2(c_x,c_y)
+
+L_pad_pos = pygame.Vector2(0,screen.get_height() / 2)
+dir_vec_L = pygame.Vector2(0,0)
+
+R_pad_pos = pygame.Vector2(screen.get_width() - 30 ,screen.get_height() / 2)
+dir_vec_R = pygame.Vector2(0,0)
+
+ball_dir = pygame.Vector2(dir_x,dir_y)
 
 # Create paddles and ball
-L_pad = Paddle([0, bounds.height / 2], [0, 0])
-R_pad = Paddle([bounds.width - Paddle.image.get_width(), bounds.height / 2], [0, 0])
-ball = Ball([bounds.width / 2, bounds.height / 2], [dir_vec_x, dir_vec_y])
+L_pad = Paddle(L_pad_pos,dir_vec_L,white,3)
+R_pad = Paddle(R_pad_pos,dir_vec_R,white,3)
+ball = Ball(center,10,ball_dir,white)
 
-# Scale images
-L_pad.scale_image(0.5)
-R_pad.scale_image(0.5)
-ball.scale_image(0.5)
+units = [L_pad,R_pad,ball]
 
 # Main game loop
 done = False
@@ -146,36 +171,56 @@ while not done:
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
-                done = False
+                done = True
 
-    # Game logic
-    screen.fill(dark_blue)
 
-    # Load and display the scaled images
-    L_pad.load(Paddle.image,screen)
-    R_pad.load(Paddle.image,screen)
-    ball.load(Ball.image,screen)
-    
-    #move pallets
+    # move paddles
     keys = pygame.key.get_pressed()
 
     # Player 2 controls
     if keys[pygame.K_UP]:
-        R_pad.position["y"] -= 3
+        R_pad.dir_vec.y -= 1
+        R_pad.is_moving =  True
     elif keys[pygame.K_DOWN]:
-        R_pad.position["y"] += 3
+        R_pad.dir_vec.y += 1
+        R_pad.is_moving =  True
+        
+    else:
+        R_pad.dir_vec.y = 0
+        R_pad.is_moving =  False
+        
 
     # Player 1 controls
     if keys[pygame.K_z]:
-        L_pad.position["y"] -= 3
+        L_pad.dir_vec.y -= 1
+        L_pad.is_moving = True
     elif keys[pygame.K_s]:
-        L_pad.position["y"] += 3
+        L_pad.dir_vec.y += 1
+        L_pad.is_moving = True
+        
+    else:
+        L_pad.dir_vec.y = 0
+        
+        L_pad.is_moving = False
 
-    Collision.collide_with(L_pad,R_pad,ball)
 
-    # Move the ball
-    ball.position["x"] += ball.dir_vect["x"]
-    ball.position["y"] += ball.dir_vect["y"]
+
+    # Game logic
+    screen.fill(dark_blue)
+
+
+    # update
+   
+    Collision.collide_with_boundary(ball)
+    # Check collision with paddles
+    ball.check_collision_with_left_paddle(L_pad)
+    ball.check_collision_with_right_paddle(R_pad)
+
+
+    # draw
+    for unit in units:
+        unit.update()
+        unit.draw(screen)
     
     pygame.display.flip()
     pygame.time.Clock().tick(60)
