5c36381d41dbef3a44cfb0f6aef2af298844b0c5
davidquie12 <56687064+davidquie12@users.noreply.github.com>
2023-11-21 20:25:48 +0100

    states implemented

--- venv/src/pong.py
+++ venv/src/pong.py
@@ -2,6 +2,9 @@ import pygame
 from collections import namedtuple
 from random import randint, uniform
 import math
+from pygwidgets import TextButton
+
+window = pygame.Vector2(640,640)
 
 class Unit:
     def __init__(self,pos : pygame.Vector2,dir_vec : pygame.Vector2,color,speed,
@@ -14,20 +17,17 @@ class Unit:
         self.speed = speed
         self.dir_vec = dir_vec
         
-    def draw(self,screen : pygame.surface):
+    def draw(self):
         pass
     
     def update(self):
         pass
     
-        
-
-    
+     
 class Paddle(Unit):
     
     def __init__(self,pos : pygame.Vector2,dir_vec : pygame.Vector2,color,speed):
         super().__init__(pos,dir_vec,color,speed)
-        self.game = game 
         self.pos = pos 
         self.dir_vec = dir_vec
         self.color = color
@@ -46,8 +46,8 @@ class Paddle(Unit):
         # Check and adjust if the paddle hits the screen boundaries
         if self.rect.top <= 0:
             self.rect.y = 0
-        elif self.rect.bottom >= screen.get_height():
-            self.rect.y = screen.get_height() - self.height
+        elif self.rect.bottom >= window.x:
+            self.rect.y = window.y - self.height
         
 class Ball(Unit):
     
@@ -75,8 +75,7 @@ class Ball(Unit):
                 else:
                     self.speed = 4
 
-            
-
+        
     def check_collision_with_right_paddle(self, R_pad: Paddle):
         if (
             self.dir_vec.x > 0  # Check if the ball is moving right
@@ -87,142 +86,219 @@ class Ball(Unit):
             # Check for top/bottom collision
             if self.center.y - self.radius <= R_pad.rect.top or self.center.y + self.radius >= R_pad.rect.bottom:
                 self.dir_vec.y *= -1  
-                if L_pad.is_moving:
+                if R_pad.is_moving:
                     self.speed = 10
                 else:
                     self.speed = 4
                 
-
-
-        
-    def draw(self,screen : pygame.surface):
-        pygame.draw.circle(screen,self.color,self.center,self.radius)
+   
+    def draw(self,window : pygame.surface):
+        pygame.draw.circle(window,self.color,self.center,self.radius)
         
     def update(self):
         self.pos.x += (self.dir_vec.x * self.speed)
         self.pos.y += (self.dir_vec.y * self.speed)
-        
         
 class Collision:      
     @staticmethod
     def collide_with_boundary(ball: Ball):
         
         if ball.center.x - ball.radius <= 0 or \
-            ball.center.x + ball.radius >= screen.get_width():
-                pygame.quit()
+            ball.center.x + ball.radius >= window.x:
+                return True
+
                 
         if ball.center.y - ball.radius <= 0 or \
-            ball.center.y + ball.radius >= screen.get_height():
+            ball.center.y + ball.radius >= window.y:
                 ball.dir_vec.y *= -1
                 
+                
+class Play:
+    def __init__(self, screen : pygame.Surface):
+        self.screen = screen
+        self.game_over = False
+        # Set up colors
+        self.dark_blue = (0, 0, 145)
+        self.white = (255, 255, 255)
+
+        # Create random generator
+        self.random_angle = uniform(0, 2 * math.pi)
+        self.dir_x = math.cos(self.random_angle)
+        self.dir_y = math.sin(self.random_angle)
+
+        # Random start pos
+        self.center = pygame.Vector2(randint(30, 600), randint(30, 600))
+
+        self.L_pad_pos = pygame.Vector2(0, self.screen.get_height() / 2)
+        self.dir_vec_L = pygame.Vector2(0, 0)
+
+        self.R_pad_pos = pygame.Vector2(self.screen.get_width() - 30, self.screen.get_height() / 2)
+        self.dir_vec_R = pygame.Vector2(0, 0)
+
+        self.ball_dir = pygame.Vector2(self.dir_x, self.dir_y)
+
+        # Create paddles and ball
+        self.L_pad = Paddle(self.L_pad_pos, self.dir_vec_L, self.white, 3)
+        self.R_pad = Paddle(self.R_pad_pos, self.dir_vec_R, self.white, 3)
+        self.ball = Ball(self.center, 10, self.ball_dir, self.white)
+
+        self.units = [self.L_pad, self.R_pad, self.ball]
+
+    def update(self,screen):
+        keys = pygame.key.get_pressed()
+
+        # Player 2 controls
+        if keys[pygame.K_UP]:
+            self.R_pad.dir_vec.y -= 1
+            self.R_pad.is_moving = True
+        elif keys[pygame.K_DOWN]:
+            self.R_pad.dir_vec.y += 1
+            self.R_pad.is_moving = True
+        else:
+            self.R_pad.dir_vec.y = 0
+            self.R_pad.is_moving = False
+
+        # Player 1 controls
+        if keys[pygame.K_z]:
+            self.L_pad.dir_vec.y -= 1
+            self.L_pad.is_moving = True
+        elif keys[pygame.K_s]:
+            self.L_pad.dir_vec.y += 1
+            self.L_pad.is_moving = True
+        else:
+            self.L_pad.dir_vec.y = 0
+            self.L_pad.is_moving = False
+
+        screen.fill(self.dark_blue)
+
+        # Update game logic
+        self.game_over = Collision.collide_with_boundary(self.ball)
+        self.ball.check_collision_with_left_paddle(self.L_pad)
+        self.ball.check_collision_with_right_paddle(self.R_pad)
+
+        # Draw the game elements
+        for unit in self.units:
+            unit.update()
+            unit.draw(self.screen)
+            
+    def process_event(self):
+        if self.game_over:
+            return True
+
+        
         
 
 
+class State:
+    def process_event(self, event):
+        pass
 
-# Initialize Pygame
-game = pygame.init()
+    def update(self,screen : pygame.surface):
+        pass
 
-# Set up the screen
-
-screen = pygame.display.set_mode((640, 640))
-
-# Set up colors
-Color = namedtuple("Color", ("x", "y", "z"))
-dark_blue = (0, 0, 145)
-white = (0,0,0)
-
-# create random generator
-# Generate random angles between 0 and 2 * pi
-random_angle = uniform(0, 2 * math.pi)
-
-# Calculate the x and y components of the unit vector using trigonometry
-dir_x = math.cos(random_angle) 
-dir_y = math.sin(random_angle) 
-
-# random start pos
-c_x = randint(30,600) 
-c_y = randint(30,600) 
-
-center = pygame.Vector2(c_x,c_y)
-
-L_pad_pos = pygame.Vector2(0,screen.get_height() / 2)
-dir_vec_L = pygame.Vector2(0,0)
-
-R_pad_pos = pygame.Vector2(screen.get_width() - 30 ,screen.get_height() / 2)
-dir_vec_R = pygame.Vector2(0,0)
-
-ball_dir = pygame.Vector2(dir_x,dir_y)
-
-# Create paddles and ball
-L_pad = Paddle(L_pad_pos,dir_vec_L,white,3)
-R_pad = Paddle(R_pad_pos,dir_vec_R,white,3)
-ball = Ball(center,10,ball_dir,white)
-
-units = [L_pad,R_pad,ball]
-
-# Main game loop
-done = False
-
-while not done:
-    # Inside the main game loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+class PlayingState(State):
+    def __init__(self,screen):
+        self.should_exit = False
+        self.next_state = None
+        self.game =  Play(screen)
         
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                done = True
+    def process_event(self, event):
+        if self.game.process_event():
+            self.next_state = GameOverState()
+         
+
+    def update(self,screen):
+        # Update logica voor het speelscherm
+        self.game.update(screen)
+
+    # Voeg andere functionaliteiten toe die nodig zijn voor het speelscherm
 
 
-    # move paddles
-    keys = pygame.key.get_pressed()
-
-    # Player 2 controls
-    if keys[pygame.K_UP]:
-        R_pad.dir_vec.y -= 1
-        R_pad.is_moving =  True
-    elif keys[pygame.K_DOWN]:
-        R_pad.dir_vec.y += 1
-        R_pad.is_moving =  True
+class GameOverState(State):
+    def __init__(self):
+        self.should_exit = False
+        self.next_state = None
         
-    else:
-        R_pad.dir_vec.y = 0
-        R_pad.is_moving =  False
-        
-
-    # Player 1 controls
-    if keys[pygame.K_z]:
-        L_pad.dir_vec.y -= 1
-        L_pad.is_moving = True
-    elif keys[pygame.K_s]:
-        L_pad.dir_vec.y += 1
-        L_pad.is_moving = True
-        
-    else:
-        L_pad.dir_vec.y = 0
-        
-        L_pad.is_moving = False
+        self.font = pygame.font.Font(None, 80)  # You can adjust the font size here
+        self.game_over_text = self.font.render("Game Over", True, (255, 0, 0))  # Red color
 
 
+    def process_event(self, event):
+        # Verwerk gebeurtenissen voor het game over-scherm
+        pass
 
-    # Game logic
-    screen.fill(dark_blue)
+    def update(self, screen):
+        screen.fill((0, 0, 0))  # Fill the screen with black
 
+        # Calculate the text position to center it
+        text_rect = self.game_over_text.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2))
 
-    # update
-   
-    Collision.collide_with_boundary(ball)
-    # Check collision with paddles
-    ball.check_collision_with_left_paddle(L_pad)
-    ball.check_collision_with_right_paddle(R_pad)
+        # Blit the "Game Over" text onto the screen
+        screen.blit(self.game_over_text, text_rect)
 
+    # Voeg andere functionaliteiten toe die nodig zijn voor het game over-scherm
 
-    # draw
-    for unit in units:
-        unit.update()
-        unit.draw(screen)
+class MainMenuState(State):
+    def __init__(self,screen):
+        self.should_exit = False
+        self.next_state = None
+        self.screen = screen
+        # Maak knoppen voor het hoofdmenu
+        self.start_button = TextButton(window=screen,loc=(200,200), text='Start game' ,fontSize=50,textColor=(125,125,125))
+        self.exit_button = TextButton(window=screen,loc=(200,400), text='Exit game', fontSize=50, textColor=(125,125,125))
+
+    def process_event(self, event):
+        # Verwerk gebeurtenissen voor het hoofdmenu
+        if self.start_button.handleEvent(event):
+            # Logica om het spel te starten
+            self.next_state = PlayingState(self.screen)
+        elif self.exit_button.handleEvent(event):
+            self.should_exit = True
+
+    def update(self,screen):
+        # Update logica voor het hoofdmenu
+        screen.fill((0, 0, 0))  # Fill the screen with black
+        # Draw the buttons onto the screen
+        self.start_button.draw()
+        self.exit_button.draw()
+
+    # Voeg andere functionaliteiten toe die nodig zijn voor het hoofdmenu
+
+# De main methode
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640,640))
+    pygame.display.set_caption("pong")
+    game_state = MainMenuState(screen=screen)  # Begin met het hoofdmenu
     
-    pygame.display.flip()
-    pygame.time.Clock().tick(60)
+    
+    running = True
 
-pygame.quit()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    running = False
+
+        game_state.process_event(event)
+        game_state.update(screen)
+
+        pygame.display.flip()
+        pygame.time.Clock().tick(60)
+
+        if game_state.should_exit:
+            running = False
+
+        if game_state.next_state:
+            game_state = game_state.next_state
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
+                
+
+
